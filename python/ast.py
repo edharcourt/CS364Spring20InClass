eb244369be99e793de5a36303ec63c8a718f43d8
Ed Harcourt <ehar@stlawu.edu>
2020-04-20 11:59:32 -0400

    .

--- python/ast.py
+++ python/ast.py
@@ -3,10 +3,38 @@ SLU-C Abstract Syntax Trees
 
 An abstract syntax tree (AST) is a data structure that represents
 the concrete (text) syntax of a program
+
+Using a class hierarchy to represent language features in the grammar
+is called the "interpreter pattern"
+
+Singleton
+Strategy Pattern  (sorting strategy)
+Interpreter Pattern
+
+design patterns - catalog of best practices in software design
+
 """
-from typing import Sequence, Union
+from typing import Sequence, Union, Optional
 
 # Use a class hierarchy to represent types.
+class Expr:
+    """
+    Base class for expressions
+    """
+    pass
+
+class Type:
+    pass
+
+class IntegerType(Type):
+    pass
+
+class FloatType(Type):
+    pass
+
+class BoolType(Type):
+    pass
+
 
 class FunctionDef:
     def __init__(self, t, id:str, params, decls, stmts):
@@ -19,6 +47,30 @@ class FunctionDef:
     def __str__(self):
         pass
 
+    def eval(self) -> Union[int, float, bool]:
+        # an environment maps identifiers to values
+        # parameters or local variables
+        # to evaluate a function you evaluate all of the statements
+        # within the environment
+        env = {}   # TODO Fix this
+        for s in self.stmts:
+            s.eval(env)  # TODO define environment
+
+class Stmt:
+    pass
+
+class IfStmt(Stmt):
+    def __init__(self, cond: Expr, truepart: Stmt, falsepart : Optional[Stmt]):
+        pass
+
+    def eval(self, env):
+
+        if self.cond.eval():
+            self.truepart.eval(env)
+        elif self.falsepart is not None:
+            self.falsepart.eval(env)
+
+
 class Declaration:
     pass
 
@@ -28,11 +80,6 @@ class Program:
         self.funcs = funcs
 
 
-class Expr:
-    """
-    Base class for expressions
-    """
-    pass
 
 # TODO Don't just cut-and-paste new operations, abstract!
 
@@ -46,6 +93,40 @@ class AndExpr(Expr):
 
     def __str__(self):
         return "({0} && {1})".format(str(self.left), str(self.right))
+
+
+    # def typeof(self) -> str: # where string is 'int' or 'float' or 'bool' or 'error'
+    # strings are not abstract "INT" "int" "fred"
+    # Expressions have types
+    #def typeof(self) -> Type:
+    def typeof(self) -> Union[int, bool, float]:   #
+        """
+        Return the type of the expression.
+        1) 4 + 4 is an int
+        2) 2 * 3.14 is a float
+        3) True && False is a bool
+        4) True && 3.14 type error
+        Static type checking: type check the program *before* we evaluate it.
+        Scheme is dynamically type checked. Type errors checked at run time.
+        Java - statically type checked.
+        C - static
+        Python - dynamic, checked at run time
+                 mypy - static type checker for Python
+        """
+        if self.left.typeof() == BoolType and self.right.typeof() == BoolType:
+            return BoolType
+        else:
+            # type error
+            raise SLUCTypeError(
+                "type error on line {0}, expected two booleans got a {1} and a {2}".format(0))
+
+class SLUCTypeError(Exception):
+    def __init__(self, message: str):
+        Exception.__init__(self)
+        self.message = message
+
+    def __str__(self):
+        return self.message
 
 class AddExpr(Expr):
     def __init__(self, left: Expr, right: Expr):
@@ -86,6 +167,12 @@ class MultExpr(Expr):
 
     def eval(self) -> Union[int,float]:
         # TODO environment
+        # Implementing SLU-C multiplication using Python's multiplication
+        # implmented * using mul instruction
+
+        # If we checked type when running eval we have a "dynamically typed"
+        # language
+
         return self.left.eval() *  self.right.eval()
 
 
@@ -118,6 +205,12 @@ class IDExpr(Expr):
         # env is a dictionary
         pass
 
+    def typeof(self, decls) -> Type:
+        # TODO type decls appropriately as a dictionary type
+        # look up the variable type in the declaration dictoinary
+        # from the function definition (FunctionDef)
+        pass
+
 class IntLitExpr(Expr):
 
     def __init__(self, intlit: str):
@@ -131,6 +224,13 @@ class IntLitExpr(Expr):
 
     def eval(self):
         return self.intlit   # base case
+
+    #def typeof(self) -> Type:
+    # representing SLU-C types using Python types
+    def typeof(self) -> type:
+
+        #return IntegerType
+        return int
 
 if __name__ == '__main__':
     """
